ebe8eff46534f2f15daaa3e89266555586b92ca3
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-21 10:35:20 -0600

    primera version

---
+++ Ejercicio2.py
@@ -0,0 +1,9 @@
+#Operación con 3 tipos de operadores
+a = float(input("a ->"))
+b = float(input("a ->"))
+
+resultado = ((3+5*8)<3 and ((-6/3*4)+2<2)) or (a>b)
+
+print(f"El resultado es {resultado}")
+
+#Carolina EM
